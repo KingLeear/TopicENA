ca352a8aa54a9f4901e68a071a4437d77fba97a0
owen198 <cfleu198@gmail.com>
2026-01-13 17:06:41 +0800

    fix the example

--- r/ena_example.R
+++ r/ena_example.R
@@ -47,62 +47,64 @@ set.ena = ena(
 
 
 
-# ena.plot(set.ena, title = "FirstGame mean plot") |>
-#   ena.plot.network(network = first.game.mean, colors = c("red"))
-# 
-# ena.plot(set.ena, title = "SecondGame mean plot") |>
-#   ena.plot.network(network = second.game.mean, colors = c("blue"))
-# 
-# 
-# 
-# # Subset lineweights for FirstGame
-# first.game.lineweights = as.matrix(set.ena$line.weights$Condition$FirstGame)
-# # Subset lineweights for SecondGame
-# second.game.lineweights = as.matrix(set.ena$line.weights$Condition$SecondGame)
-# 
-# first.game.mean = as.vector(colMeans(first.game.lineweights))
-# second.game.mean = as.vector(colMeans(second.game.lineweights))
-# subtracted.mean = first.game.mean - second.game.mean
-# 
-# ena.plot(
-#   set.ena,
-#   title = "Subtracted: FirstGame (red) - SecondGame (blue)"
-# ) |>
-#   ena.plot.network(
-#     network = subtracted.mean * 5,  # optional rescaling
-#     colors = c("red", "blue")
-#   )
-# 
-# 
-# 
-# 
-# first.game.points = as.matrix(set.ena$points$Condition$FirstGame)
-# 
-# ena.plot(set.ena, title = "FirstGame mean network and its points") |>
-#   ena.plot.network(network = first.game.mean, colors = c("red")) |>
-#   ena.plot.points(points = first.game.points, colors = c("red")) |>
-#   ena.plot.group(point = first.game.points, colors =c("red"),
-#                  confidence.interval = "box")
-# 
-# second.game.points = as.matrix(set.ena$points$Condition$SecondGame)
-# 
-# ena.plot(set.ena, title = "SecondGame mean network and its points") |>
-#   ena.plot.network(network = second.game.mean, colors = c("blue")) |>
-#   ena.plot.points(points = second.game.points, colors = c("blue")) |>
-#   ena.plot.group(point = second.game.points, colors =c("blue"),
-#                  confidence.interval = "box")
-# 
-# 
-# ena.plot(set.ena, title = "Subtracted mean network: FirstGame (red) - SecondG
-# ame (blue)") |>
-#   ena.plot.network(network = subtracted.mean * 5,
-#                    colors = c("red", "blue")) |>
-#   ena.plot.points(points = first.game.points, colors = c("red")) |>
-#   ena.plot.group(point = first.game.points, colors =c("red"),
-#                  confidence.interval = "box") |>
-#   ena.plot.points(points = second.game.points, colors = c("blue")) |>
-#   ena.plot.group(point = second.game.points, colors =c("blue"),
-#                  confidence.interval = "box")
+
+
+
+first.game.lineweights = as.matrix(set.ena$line.weights$Condition$FirstGame)
+second.game.lineweights = as.matrix(set.ena$line.weights$Condition$SecondGame)
+
+first.game.mean = as.vector(colMeans(first.game.lineweights))
+second.game.mean = as.vector(colMeans(second.game.lineweights))
+subtracted.mean = first.game.mean - second.game.mean
+
+
+ena.plot(set.ena, title = "FirstGame mean plot") |>
+  ena.plot.network(network = first.game.mean, colors = c("red"))
+
+ena.plot(set.ena, title = "SecondGame mean plot") |>
+  ena.plot.network(network = second.game.mean, colors = c("blue"))
+
+
+
+ena.plot(
+  set.ena,
+  title = "Subtracted: FirstGame (red) - SecondGame (blue)"
+) |>
+  ena.plot.network(
+    network = subtracted.mean * 5,  # optional rescaling
+    colors = c("red", "blue")
+  )
+
+
+
+
+first.game.points = as.matrix(set.ena$points$Condition$FirstGame)
+
+ena.plot(set.ena, title = "FirstGame mean network and its points") |>
+  ena.plot.network(network = first.game.mean, colors = c("red")) |>
+  ena.plot.points(points = first.game.points, colors = c("red")) |>
+  ena.plot.group(point = first.game.points, colors =c("red"),
+                 confidence.interval = "box")
+
+second.game.points = as.matrix(set.ena$points$Condition$SecondGame)
+
+ena.plot(set.ena, title = "SecondGame mean network and its points") |>
+  ena.plot.network(network = second.game.mean, colors = c("blue")) |>
+  ena.plot.points(points = second.game.points, colors = c("blue")) |>
+  ena.plot.group(point = second.game.points, colors =c("blue"),
+                 confidence.interval = "box")
+
+
+ena.plot(set.ena, title = "Subtracted mean network: FirstGame (red) - SecondG
+ame (blue)") |>
+  ena.plot.network(network = subtracted.mean * 5,
+                   colors = c("red", "blue")) |>
+  ena.plot.points(points = first.game.points, colors = c("red")) |>
+  ena.plot.group(point = first.game.points, colors =c("red"),
+                 confidence.interval = "box") |>
+  ena.plot.points(points = second.game.points, colors = c("blue")) |>
+  ena.plot.group(point = second.game.points, colors =c("blue"),
+                 confidence.interval = "box")
 
 
 
--- r/ena_example_csv.R
+++ r/ena_example_csv.R
@@ -7,17 +7,14 @@ library(rENA)
 
 # load example dataset from rENA
 # data = rENA::RS.data
-data <- read.csv("topic_keywords_0.3.csv")
+data <- read.csv("../outputs/topic_keywords_0.05.csv")
 
 unitCols = c("Condition", "UserName")
 
-codesCols = c(
-  "year.game",
-  "space.nasa",
-  "israel.israeli",
-  "jpeg.file",
-  "armenian.armenians",
-  "mac.modem"
+headers <- colnames(data)
+codesCols <- setdiff(
+  headers,
+  c("Condition", "ActivityNumber", "UserName", "text")
 )
 
 conversationCols = c(
@@ -28,7 +25,7 @@ conversationCols = c(
 
 
 groupsVar = "Condition"
-groups = c("FirstGame", "SecondGame")
+groups = c("HDSE", "LDSE")
 
 
 
@@ -37,73 +34,84 @@ set.ena = ena(
   units = unitCols,
   codes = codesCols,
   conversation = conversationCols,
-  window.size.back = 7,
+  window.size.back = 2,
   groupVar = groupsVar,
   groups = groups,
   mean = TRUE
-  
 )
 
 
 
 
 
-# ena.plot(set.ena, title = "FirstGame mean plot") |>
-#   ena.plot.network(network = first.game.mean, colors = c("red"))
-# 
-# ena.plot(set.ena, title = "SecondGame mean plot") |>
-#   ena.plot.network(network = second.game.mean, colors = c("blue"))
-# 
-# 
-# 
-# # Subset lineweights for FirstGame
-# first.game.lineweights = as.matrix(set.ena$line.weights$Condition$FirstGame)
-# # Subset lineweights for SecondGame
-# second.game.lineweights = as.matrix(set.ena$line.weights$Condition$SecondGame)
-# 
-# first.game.mean = as.vector(colMeans(first.game.lineweights))
-# second.game.mean = as.vector(colMeans(second.game.lineweights))
-# subtracted.mean = first.game.mean - second.game.mean
-# 
-# ena.plot(
-#   set.ena,
-#   title = "Subtracted: FirstGame (red) - SecondGame (blue)"
-# ) |>
-#   ena.plot.network(
-#     network = subtracted.mean * 5,  # optional rescaling
-#     colors = c("red", "blue")
-#   )
-# 
-# 
-# 
-# 
-# first.game.points = as.matrix(set.ena$points$Condition$FirstGame)
-# 
-# ena.plot(set.ena, title = "FirstGame mean network and its points") |>
-#   ena.plot.network(network = first.game.mean, colors = c("red")) |>
-#   ena.plot.points(points = first.game.points, colors = c("red")) |>
-#   ena.plot.group(point = first.game.points, colors =c("red"),
-#                  confidence.interval = "box")
-# 
-# second.game.points = as.matrix(set.ena$points$Condition$SecondGame)
-# 
-# ena.plot(set.ena, title = "SecondGame mean network and its points") |>
-#   ena.plot.network(network = second.game.mean, colors = c("blue")) |>
-#   ena.plot.points(points = second.game.points, colors = c("blue")) |>
-#   ena.plot.group(point = second.game.points, colors =c("blue"),
-#                  confidence.interval = "box")
-# 
-# 
-# ena.plot(set.ena, title = "Subtracted mean network: FirstGame (red) - SecondG
-# ame (blue)") |>
-#   ena.plot.network(network = subtracted.mean * 5,
-#                    colors = c("red", "blue")) |>
-#   ena.plot.points(points = first.game.points, colors = c("red")) |>
-#   ena.plot.group(point = first.game.points, colors =c("red"),
-#                  confidence.interval = "box") |>
-#   ena.plot.points(points = second.game.points, colors = c("blue")) |>
-#   ena.plot.group(point = second.game.points, colors =c("blue"),
-#                  confidence.interval = "box")
+
+
+
+
+ena.plot(set.ena, title = "FirstGame mean plot") |>
+  ena.plot.network(network = , colors = c("red"))
+
+ena.plot(set.ena, title = "SecondGame mean plot") |>
+  ena.plot.network(network = LDSE.mean, colors = c("blue"))
+
+
+
+# Subset lineweights for FirstGame
+first.game.lineweights = as.matrix(set.ena$line.weights$Condition$FirstGame)
+# Subset lineweights for SecondGame
+second.game.lineweights = as.matrix(set.ena$line.weights$Condition$SecondGame)
+
+first.game.mean = as.vector(colMeans(first.game.lineweights))
+second.game.mean = as.vector(colMeans(second.game.lineweights))
+subtracted.mean = first.game.mean - second.game.mean
+
+ena.plot(
+  set.ena,
+  title = "Subtracted: FirstGame (red) - SecondGame (blue)"
+) |>
+  ena.plot.network(
+    network = subtracted.mean * 5,  # optional rescaling
+    colors = c("red", "blue")
+  )
+
+
+
+
+first.game.points = as.matrix(set.ena$points$Condition$FirstGame)
+
+ena.plot(set.ena, title = "FirstGame mean network and its points") |>
+  ena.plot.network(network = first.game.mean, colors = c("red")) |>
+  ena.plot.points(points = first.game.points, colors = c("red")) |>
+  ena.plot.group(point = first.game.points, colors =c("red"),
+                 confidence.interval = "box")
+
+second.game.points = as.matrix(set.ena$points$Condition$SecondGame)
+
+ena.plot(set.ena, title = "SecondGame mean network and its points") |>
+  ena.plot.network(network = second.game.mean, colors = c("blue")) |>
+  ena.plot.points(points = second.game.points, colors = c("blue")) |>
+  ena.plot.group(point = second.game.points, colors =c("blue"),
+                 confidence.interval = "box")
+
+
+ena.plot(set.ena, title = "Subtracted mean network: FirstGame (red) - SecondG
+ame (blue)") |>
+  ena.plot.network(network = subtracted.mean * 5,
+                   colors = c("red", "blue")) |>
+  ena.plot.points(points = first.game.points, colors = c("red")) |>
+  ena.plot.group(point = first.game.points, colors =c("red"),
+                 confidence.interval = "box") |>
+  ena.plot.points(points = second.game.points, colors = c("blue")) |>
+  ena.plot.group(point = second.game.points, colors =c("blue"),
+                 confidence.interval = "box")
+
+
+
+
+
+
+
+
 
 
 
@@ -114,5 +122,5 @@ plot = ena.plotter(set.ena,
                    network = T,
                    print.plots = T,
                    groupVar = "Condition",
-                   groups = c("SecondGame","FirstGame"),
+                   groups = c("HDSE","LDSE"),
                    subtractionMultiplier = 5)
